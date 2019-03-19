9a617f42371d4ea30ee3a45ea097b5f3fcd0c345
edsilv <e.silverton@gmail.com>
2019-03-19 13:37:39 +0000

    added all actions

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,4 +1,7 @@
 import { Tool } from "../interfaces/Tool";
+import { DisplayMode } from "../enums/DisplayMode";
+import { Orientation } from "../enums/Orientation";
+import { ToolType } from "../enums/ToolType";
 
 export interface NullAction {
   type: TypeKeys.NULL
@@ -14,18 +17,54 @@ export type ActionTypes =
   | AppSelectToolAction
   | AppUpdateToolAction
   | AppSaveToolsAction
+  | AppSetDisplayAction
+  | AppSetOrientationAction
+  | AppSetToolsVisibleAction
+  | AppSetToolsEnabledAction
+  | AppSetToolTypeAction
+  | AppSetOptionsVisibleAction
+  | AppSetOptionsEnabledAction
+  | AppSetBoundingBoxVisibleAction
+  | AppSetTHREEJSSceneNeedsUpdateAction
+  | AppSetSlicesIndexAction
+  | AppSetSlicesWindowWidthAction
+  | AppSetSlicesWindowCenterAction
+  | AppSetVolumeStepsAction
+  | AppSetVolumeWindowWidthAction
+  | AppSetVolumeWindowCenterAction
+  | AppSetAngleToolEnabledAction
+  | AppSetAnnotationToolEnabledAction
+  | AppSetRulerToolEnabledAction
 ;
 
 export enum TypeKeys {
-  NULL = 'NULL',
-  ERROR = 'ERROR',
-  APP_SET_SRC = 'APP_SET_SRC',
-  APP_SET_SRC_LOADED = 'APP_SET_SRC_LOADED',
-  APP_ADD_TOOL = 'APP_ADD_TOOL',
-  APP_REMOVE_TOOL = 'APP_REMOVE_TOOL',
-  APP_SELECT_TOOL = 'APP_SELECT_TOOL',
-  APP_UPDATE_TOOL = 'APP_UPDATE_TOOL',
-  APP_SAVE_TOOLS = 'APP_SAVE_TOOLS'
+  NULL = "NULL",
+  ERROR = "ERROR",
+  APP_SET_SRC = "APP_SET_SRC",
+  APP_SET_SRC_LOADED = "APP_SET_SRC_LOADED",
+  APP_ADD_TOOL = "APP_ADD_TOOL",
+  APP_REMOVE_TOOL = "APP_REMOVE_TOOL",
+  APP_SELECT_TOOL = "APP_SELECT_TOOL",
+  APP_UPDATE_TOOL = "APP_UPDATE_TOOL",
+  APP_SAVE_TOOLS = "APP_SAVE_TOOLS",
+  APP_SET_DISPLAY = "APP_SET_DISPLAY",
+  APP_SET_ORIENTATION = "APP_SET_ORIENTATION",
+  APP_SET_TOOLS_VISIBLE = "APP_SET_TOOLS_VISIBLE",
+  APP_SET_TOOLS_ENABLED = "APP_SET_TOOLS_ENABLED",
+  APP_SET_TOOL_TYPE = "APP_SET_TOOL_TYPE",
+  APP_SET_OPTIONS_VISIBLE = "APP_SET_OPTIONS_VISIBLE",
+  APP_SET_OPTIONS_ENABLED = "APP_SET_OPTIONS_ENABLED",
+  APP_SET_BOUNDINGBOX_VISIBLE = "APP_SET_BOUNDINGBOX_VISIBLE",
+  APP_SET_THREEJS_SCENE_NEEDS_UPDATE = "APP_SET_THREEJS_SCENE_NEEDS_UPDATE",
+  APP_SET_SLICES_INDEX = "APP_SET_SLICES_INDEX",
+  APP_SET_SLICES_WINDOW_WIDTH = "APP_SET_SLICES_WINDOW_WIDTH",
+  APP_SET_SLICES_WINDOW_CENTER = "APP_SET_SLICES_WINDOW_CENTER",
+  APP_SET_VOLUME_STEPS = "APP_SET_VOLUME_STEPS",
+  APP_SET_VOLUME_WINDOW_WIDTH = "APP_SET_VOLUME_WINDOW_WIDTH",
+  APP_SET_VOLUME_WINDOW_CENTER = "APP_SET_VOLUME_WINDOW_CENTER",
+  APP_SET_ANGLE_TOOL_ENABLED = "APP_SET_ANGLE_TOOL_ENABLED",
+  APP_SET_ANNOTATION_TOOL_ENABLED = "APP_SET_ANNOTATION_TOOL_ENABLED",
+  APP_SET_RULER_TOOL_ENABLED = "APP_SET_RULER_TOOL_ENABLED",
 };
 
 export interface AppSetSrcAction {
@@ -37,7 +76,7 @@ export const appSetSrc = (payload: string) => async (dispatch, _getState) => {
   return dispatch({
     type: TypeKeys.APP_SET_SRC,
     payload: payload
-  })
+  });
 };
 
 export interface AppSetSrcLoadedAction {
@@ -49,7 +88,7 @@ export const appSetSrcLoaded = (payload: boolean) => async (dispatch, _getState)
   return dispatch({
     type: TypeKeys.APP_SET_SRC_LOADED,
     payload: payload
-  })
+  });
 };
 
 export interface AppAddToolAction {
@@ -61,7 +100,7 @@ export const appAddTool = (payload: Tool) => async (dispatch, _getState) => {
   return dispatch({
     type: TypeKeys.APP_ADD_TOOL,
     payload: payload
-  })
+  });
 };
 
 export interface AppRemoveToolAction {
@@ -73,7 +112,7 @@ export const appRemoveTool = (payload: number) => async (dispatch, _getState) =>
   return dispatch({
     type: TypeKeys.APP_REMOVE_TOOL,
     payload: payload
-  })
+  });
 };
 
 export interface AppSelectToolAction {
@@ -85,7 +124,7 @@ export const appSelectTool = (payload: number) => async (dispatch, _getState) =>
   return dispatch({
     type: TypeKeys.APP_SELECT_TOOL,
     payload: payload
-  })
+  });
 };
 
 export interface AppUpdateToolAction {
@@ -97,7 +136,7 @@ export const appUpdateTool = (payload: Tool) => async (dispatch, _getState) => {
   return dispatch({
     type: TypeKeys.APP_UPDATE_TOOL,
     payload: payload
-  })
+  });
 };
 
 export interface AppSaveToolsAction {
@@ -109,5 +148,221 @@ export const appSaveTools = (payload: void) => async (dispatch, _getState) => {
   return dispatch({
     type: TypeKeys.APP_SAVE_TOOLS,
     payload: payload
-  })
+  });
+};
+
+export interface AppSetDisplayAction {
+  type: TypeKeys.APP_SET_DISPLAY,
+  payload: DisplayMode;
+}
+
+export const appSetDisplay = (payload: DisplayMode) => async (dispatch, _getState) => {
+  return dispatch({
+    type: TypeKeys.APP_SET_DISPLAY,
+    payload: payload
+  });
+};
+
+export interface AppSetOrientationAction {
+  type: TypeKeys.APP_SET_ORIENTATION,
+  payload: Orientation;
+}
+
+export const appSetOrientation = (payload: Orientation) => async (dispatch, _getState) => {
+  return dispatch({
+    type: TypeKeys.APP_SET_ORIENTATION,
+    payload: payload
+  });
+};
+
+export interface AppSetToolsVisibleAction {
+  type: TypeKeys.APP_SET_TOOLS_VISIBLE,
+  payload: boolean;
+}
+
+export const appSetToolsVisible = (payload: boolean) => async (dispatch, _getState) => {
+  return dispatch({
+    type: TypeKeys.APP_SET_TOOLS_VISIBLE,
+    payload: payload
+  });
+};
+
+export interface AppSetToolsEnabledAction {
+  type: TypeKeys.APP_SET_TOOLS_ENABLED,
+  payload: boolean;
+}
+
+export const appSetToolsEnabled = (payload: boolean) => async (dispatch, _getState) => {
+  return dispatch({
+    type: TypeKeys.APP_SET_TOOLS_ENABLED,
+    payload: payload
+  });
+};
+
+export interface AppSetToolTypeAction {
+  type: TypeKeys.APP_SET_TOOL_TYPE,
+  payload: ToolType;
+}
+
+export const appSetToolType = (payload: ToolType) => async (dispatch, _getState) => {
+  return dispatch({
+    type: TypeKeys.APP_SET_TOOL_TYPE,
+    payload: payload
+  });
+};
+
+export interface AppSetOptionsVisibleAction {
+  type: TypeKeys.APP_SET_OPTIONS_VISIBLE,
+  payload: boolean;
+}
+
+export const appSetOptionsVisible = (payload: boolean) => async (dispatch, _getState) => {
+  return dispatch({
+    type: TypeKeys.APP_SET_OPTIONS_VISIBLE,
+    payload: payload
+  });
+};
+
+export interface AppSetOptionsEnabledAction {
+  type: TypeKeys.APP_SET_OPTIONS_ENABLED,
+  payload: boolean;
+}
+
+export const appSetOptionsEnabled = (payload: boolean) => async (dispatch, _getState) => {
+  return dispatch({
+    type: TypeKeys.APP_SET_OPTIONS_ENABLED,
+    payload: payload
+  });
+};
+
+export interface AppSetBoundingBoxVisibleAction {
+  type: TypeKeys.APP_SET_BOUNDINGBOX_VISIBLE,
+  payload: boolean;
+}
+
+export const appSetBoundingBoxVisible = (payload: boolean) => async (dispatch, _getState) => {
+  return dispatch({
+    type: TypeKeys.APP_SET_BOUNDINGBOX_VISIBLE,
+    payload: payload
+  });
+};
+
+export interface AppSetTHREEJSSceneNeedsUpdateAction {
+  type: TypeKeys.APP_SET_THREEJS_SCENE_NEEDS_UPDATE,
+  payload: boolean;
+}
+
+export const appSetTHREEJSSceneNeedsUpdate = (payload: boolean) => async (dispatch, _getState) => {
+  return dispatch({
+    type: TypeKeys.APP_SET_THREEJS_SCENE_NEEDS_UPDATE,
+    payload: payload
+  });
+};
+
+export interface AppSetSlicesIndexAction {
+  type: TypeKeys.APP_SET_SLICES_INDEX,
+  payload: number;
+}
+
+export const appSetSlicesIndex = (payload: number) => async (dispatch, _getState) => {
+  return dispatch({
+    type: TypeKeys.APP_SET_SLICES_INDEX,
+    payload: payload
+  });
+};
+
+export interface AppSetSlicesWindowWidthAction {
+  type: TypeKeys.APP_SET_SLICES_WINDOW_WIDTH,
+  payload: number;
+}
+
+export const appSetSlicesWindowWidth = (payload: number) => async (dispatch, _getState) => {
+  return dispatch({
+    type: TypeKeys.APP_SET_SLICES_WINDOW_WIDTH,
+    payload: payload
+  });
+};
+
+export interface AppSetSlicesWindowCenterAction {
+  type: TypeKeys.APP_SET_SLICES_WINDOW_CENTER,
+  payload: number;
+}
+
+export const appSetSlicesWindowCenter = (payload: number) => async (dispatch, _getState) => {
+  return dispatch({
+    type: TypeKeys.APP_SET_SLICES_WINDOW_CENTER,
+    payload: payload
+  });
+};
+
+export interface AppSetVolumeStepsAction {
+  type: TypeKeys.APP_SET_VOLUME_STEPS,
+  payload: number;
+}
+
+export const appSetVolumeSteps = (payload: number) => async (dispatch, _getState) => {
+  return dispatch({
+    type: TypeKeys.APP_SET_VOLUME_STEPS,
+    payload: payload
+  });
+};
+
+export interface AppSetVolumeWindowWidthAction {
+  type: TypeKeys.APP_SET_VOLUME_WINDOW_WIDTH,
+  payload: number;
+}
+
+export const appSetVolumeWindowWidth = (payload: number) => async (dispatch, _getState) => {
+  return dispatch({
+    type: TypeKeys.APP_SET_VOLUME_WINDOW_WIDTH,
+    payload: payload
+  });
+};
+
+export interface AppSetVolumeWindowCenterAction {
+  type: TypeKeys.APP_SET_VOLUME_WINDOW_CENTER,
+  payload: number;
+}
+
+export const appSetVolumeWindowCenter = (payload: number) => async (dispatch, _getState) => {
+  return dispatch({
+    type: TypeKeys.APP_SET_VOLUME_WINDOW_CENTER,
+    payload: payload
+  });
+};
+
+export interface AppSetAngleToolEnabledAction {
+  type: TypeKeys.APP_SET_ANGLE_TOOL_ENABLED,
+  payload: boolean;
+}
+
+export const appSetAngleToolEnabled = (payload: boolean) => async (dispatch, _getState) => {
+  return dispatch({
+    type: TypeKeys.APP_SET_ANGLE_TOOL_ENABLED,
+    payload: payload
+  });
+};
+
+export interface AppSetAnnotationToolEnabledAction {
+  type: TypeKeys.APP_SET_ANNOTATION_TOOL_ENABLED,
+  payload: boolean;
+}
+
+export const appSetAnnotationToolEnabled = (payload: boolean) => async (dispatch, _getState) => {
+  return dispatch({
+    type: TypeKeys.APP_SET_ANNOTATION_TOOL_ENABLED,
+    payload: payload
+  });
+};
+
+export interface AppSetRulerToolEnabledAction {
+  type: TypeKeys.APP_SET_RULER_TOOL_ENABLED,
+  payload: boolean;
+}
+
+export const appSetRulerToolEnabled = (payload: boolean) => async (dispatch, _getState) => {
+  return dispatch({
+    type: TypeKeys.APP_SET_RULER_TOOL_ENABLED,
+    payload: payload
+  });
 };
--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import { ActionTypes, TypeKeys } from './actions';
 import { Tool } from '../interfaces/Tool';
 import { GetUtils } from '../utils/utils';
+import { DisplayMode } from '../enums/DisplayMode';
 
 interface AppState {
   src: string | null;
@@ -16,7 +17,7 @@ export const getInitialState = () => {
     srcLoaded: false,
     selectedTool: null,
     tools: []
-  }
+  };
 };
 
 export const app = (state: AppState = getInitialState(), action: ActionTypes) => {
@@ -27,19 +28,19 @@ export const app = (state: AppState = getInitialState(), action: ActionTypes) =>
         ...state,
         src: action.payload,
         srcLoaded: false
-      }
+      };
     }
     case TypeKeys.APP_SET_SRC_LOADED: {
       return {
         ...state,
         srcLoaded: action.payload
-      }
+      };
     }
     case TypeKeys.APP_ADD_TOOL: {
       return {
         ...state,
         tools: [...state.tools, action.payload]
-      }
+      };
     }
     case TypeKeys.APP_REMOVE_TOOL: {
       const index: number = GetUtils.getToolIndex(action.payload, state.tools);
@@ -50,13 +51,13 @@ export const app = (state: AppState = getInitialState(), action: ActionTypes) =>
           ...state.tools.slice(0, index),
           ...state.tools.slice(index + 1)
         ]
-      }
+      };
     }
     case TypeKeys.APP_SELECT_TOOL: {
       return {
         ...state,
         selectedTool: action.payload
-      }
+      };
     }
     case TypeKeys.APP_UPDATE_TOOL: {
       return {
@@ -69,15 +70,126 @@ export const app = (state: AppState = getInitialState(), action: ActionTypes) =>
           return {
             ...tool,
             ...action.payload
-          }
+          };
         })
-      }
+      };
     }
     case TypeKeys.APP_SAVE_TOOLS: {
       console.log(JSON.stringify(state.tools));
       return {
         ...state
-      }
+      };
+    }
+    case TypeKeys.APP_SET_DISPLAY: {
+      return {
+        ...state,
+        boundingBoxVisible: action.payload === DisplayMode.SLICES, // default to bounding box visible in slices mode
+        display: action.payload
+      };
+    }
+    case TypeKeys.APP_SET_ORIENTATION: {
+      return {
+        ...state,
+        THREEJSSceneNeedsUpdate: true,
+        slicesIndex: undefined,
+        orientation: action.payload
+      };
+    }
+    case TypeKeys.APP_SET_TOOLS_VISIBLE: {
+      return {
+        ...state,
+        toolsVisible: action.payload
+      };
+    }
+    case TypeKeys.APP_SET_TOOLS_ENABLED: {
+      return {
+        ...state,
+        toolsEnabled: action.payload
+      };
+    }
+    case TypeKeys.APP_SET_TOOL_TYPE: {
+      return {
+        ...state,
+        toolType: action.payload
+      };
+    }
+    case TypeKeys.APP_SET_OPTIONS_VISIBLE: {
+      return {
+        ...state,
+        optionsVisible: action.payload
+      };
+    }
+    case TypeKeys.APP_SET_OPTIONS_ENABLED: {
+      return {
+        ...state,
+        optionsEnabled: action.payload
+      };
+    }
+    case TypeKeys.APP_SET_BOUNDINGBOX_VISIBLE: {
+      return {
+        ...state,
+        boundingBoxVisible: action.payload
+      };
+    }
+    case TypeKeys.APP_SET_THREEJS_SCENE_NEEDS_UPDATE: {
+      return {
+        ...state,
+        THREEJSSceneNeedsUpdate: action.payload
+      };
+    }
+    case TypeKeys.APP_SET_SLICES_INDEX: {
+      return {
+        ...state,
+        slicesIndex: action.payload
+      };
+    }
+    case TypeKeys.APP_SET_SLICES_WINDOW_WIDTH: {
+      return {
+        ...state,
+        slicesWindowWidth: action.payload
+      };
+    }
+    case TypeKeys.APP_SET_SLICES_WINDOW_CENTER: {
+      return {
+        ...state,
+        slicesWindowCenter: action.payload
+      };
+    }
+    case TypeKeys.APP_SET_VOLUME_STEPS: {
+      return {
+        ...state,
+        volumeSteps: action.payload
+      };
+    }
+    case TypeKeys.APP_SET_VOLUME_WINDOW_WIDTH: {
+      return {
+        ...state,
+        volumeWindowWidth: action.payload
+      };
+    }
+    case TypeKeys.APP_SET_VOLUME_WINDOW_CENTER: {
+      return {
+        ...state,
+        volumeWindowCenter: action.payload
+      };
+    }
+    case TypeKeys.APP_SET_ANGLE_TOOL_ENABLED: {
+      return {
+        ...state,
+        angleToolEnabled: action.payload
+      };
+    }
+    case TypeKeys.APP_SET_ANNOTATION_TOOL_ENABLED: {
+      return {
+        ...state,
+        annotationToolEnabled: action.payload
+      };
+    }
+    case TypeKeys.APP_SET_RULER_TOOL_ENABLED: {
+      return {
+        ...state,
+        rulerToolEnabled: action.payload
+      };
     }
   }
 
