403a39079a0d8de1799dda9da8930bea48407a43
Edward Silverton <e.silverton@gmail.com>
2019-04-25 21:30:10 +0100

    commented setting camera state with undefined

--- src/components/aleph/aleph.tsx
+++ src/components/aleph/aleph.tsx
@@ -1418,11 +1418,11 @@ export class Aleph {
   private _addEventListeners(): void {
     window.addEventListener("keydown", this._keyDownHandler, false);
     window.addEventListener("keyup", this._keyUpHandler, false);
-    window.addEventListener(
-      "mouseup",
-      this._controlsInteractionFinishedHandler,
-      false
-    );
+    // window.addEventListener(
+    //   "mouseup",
+    //   this._controlsInteractionFinishedHandler,
+    //   false
+    // );
 
     this._scene.addEventListener(
       AlOrbitControlEvents.ANIMATION_FINISHED,
