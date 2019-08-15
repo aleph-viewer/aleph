b3cde3d6a8bf2a1dc21d50dcf0c22373e6237091
Nick Slack <nicolas.slack@gmail.com>
2019-08-15 15:08:04 +0100

    Custom AlTrackballControls implemented into TS

---
+++ src/utils/AlTrackballControls.ts
@@ -0,0 +1,576 @@
+/**
+ * @author Eberhard Graether / http://egraether.com/
+ * @author Mark Lundin 	/ http://mark-lundin.com
+ * @author Simone Manini / http://daron1337.github.io
+ * @author Luca Antiga 	/ http://lantiga.github.io
+ */
+
+export const AlTrackballState = {
+  NONE: -1,
+  ROTATE: 0,
+  ZOOM: 1,
+  PAN: 2,
+  TOUCH_ROTATE: 3,
+  TOUCH_ZOOM_PAN: 4
+};
+
+export class AlTrackballControls {
+  public object;
+  public target: THREE.Vector3;
+
+  // tslint:disable-next-line: no-any
+  private _domElement: any;
+
+  // API
+  private _enabled: boolean;
+  private _screen = {
+    left: 0,
+    top: 0,
+    width: 0,
+    height: 0
+  };
+  private _rotateSpeed: number;
+  private _zoomSpeed: number;
+  private _panSpeed: number;
+
+  private _noRotate: boolean;
+  private _noZoom: boolean;
+  private _noPan: boolean;
+
+  private _staticMoving: boolean;
+  private _dynamicDampingFactor: number;
+  private _minDistance: number;
+  private _maxDistance: number;
+
+  private _mouseButtons;
+
+  private EPS: number;
+  private _lastPosition: THREE.Vector3;
+
+  private _state: number;
+  private _eye: THREE.Vector3;
+  private _movePrev: THREE.Vector2;
+  private _moveCurr: THREE.Vector2;
+  private _lastAxis: THREE.Vector3;
+  private _lastAngle: number;
+  private _zoomStart: THREE.Vector2;
+  private _zoomEnd: THREE.Vector2;
+  private _touchZoomDistanceStart: number;
+  private _touchZoomDistanceEnd: number;
+  private _panStart: THREE.Vector2;
+  private _panEnd: THREE.Vector2;
+
+  private _target0: THREE.Vector3;
+  private _position0: THREE.Vector3;
+  private _up0: THREE.Vector3;
+
+  // events
+  //   private _changeEvent = { type: 'change' };
+  //   private _startEvent = { type: 'start' };
+  //   private _endEvent = { type: 'end' };
+
+  // tslint:disable-next-line: no-any
+  constructor(object: any, domElement: HTMLElement | Document) {
+    this.object = object;
+    this._domElement = domElement !== undefined ? domElement : document;
+
+    // ======== API ========
+    this._enabled = true;
+    this._screen = { left: 0, top: 0, width: 0, height: 0 };
+
+    this._rotateSpeed = 1.0;
+    this._zoomSpeed = 1.2;
+    this._panSpeed = 0.3;
+
+    this._noRotate = false;
+    this._noZoom = false;
+    this._noPan = false;
+
+    this._staticMoving = false;
+    this._dynamicDampingFactor = 0.2;
+
+    this._minDistance = 0;
+    this._maxDistance = Infinity;
+
+    this._mouseButtons = {
+      ROTATE: THREE.MOUSE.LEFT,
+      ZOOM: THREE.MOUSE.MIDDLE,
+      PAN: THREE.MOUSE.RIGHT
+    };
+    // =====================
+
+    // ===== internals =====
+
+    // for reset
+    this.target = new THREE.Vector3();
+    this.EPS = 0.000001;
+    this._lastPosition = new THREE.Vector3();
+
+    this._state = AlTrackballState.NONE;
+    this._eye = new THREE.Vector3();
+    this._movePrev = new THREE.Vector2();
+    this._moveCurr = new THREE.Vector2();
+    this._lastAxis = new THREE.Vector3();
+    this._lastAngle = 0;
+    this._zoomStart = new THREE.Vector2();
+    this._zoomEnd = new THREE.Vector2();
+    this._touchZoomDistanceStart = 0;
+    this._touchZoomDistanceEnd = 0;
+    this._panStart = new THREE.Vector2();
+    this._panEnd = new THREE.Vector2();
+
+    this._target0 = this.target.clone();
+    this._position0 = this.object.position.clone();
+    this._up0 = this.object.up.clone();
+
+    this._bindMethods();
+    this._addListeners();
+    this.update();
+  }
+
+  private _bindMethods() {
+    this._mouseDown = this._mouseDown.bind(this);
+    this._mouseMove = this._mouseMove.bind(this);
+    this._mouseUp = this._mouseUp.bind(this);
+    this._mouseWheel = this._mouseWheel.bind(this);
+    this._touchStart = this._touchStart.bind(this);
+    this._touchEnd = this._touchEnd.bind(this);
+    this._touchMove = this._touchMove.bind(this);
+    this._resize = this._resize.bind(this);
+  }
+
+  private _addListeners() {
+    this._domElement.addEventListener('mousedown', this._mouseDown, {
+      capture: false,
+      once: false,
+      passive: true
+    });
+    this._domElement.addEventListener('mousemove', this._mouseMove, {
+      capture: false,
+      once: false,
+      passive: true
+    });
+    this._domElement.addEventListener('mouseup', this._mouseUp, {
+      capture: false,
+      once: false,
+      passive: true
+    });
+    this._domElement.addEventListener('wheel', this._mouseWheel, {
+      capture: false,
+      once: false,
+      passive: true
+    });
+
+    this._domElement.addEventListener('touchstart', this._touchStart, {
+      capture: false,
+      once: false,
+      passive: true
+    });
+    this._domElement.addEventListener('touchend', this._touchEnd, {
+      capture: false,
+      once: false,
+      passive: true
+    });
+    this._domElement.addEventListener('touchmove', this._touchMove, {
+      capture: false,
+      once: false,
+      passive: true
+    });
+  }
+
+  private _mouseDown(event: MouseEvent) {
+    if (this._enabled === false) {
+      return;
+    }
+
+    if (this._state === AlTrackballState.NONE) {
+      switch (event.button) {
+        case this._mouseButtons.LEFT:
+          this._state = AlTrackballState.ROTATE;
+          if (!this._noRotate) {
+            this._moveCurr.copy(
+              this._getMouseOnCircle(event.pageX, event.pageY)
+            );
+            this._movePrev.copy(this._moveCurr);
+          }
+          break;
+
+        case this._mouseButtons.MIDDLE:
+          this._state = AlTrackballState.ZOOM;
+          if (!this._noZoom) {
+            this._zoomStart.copy(
+              this._getMouseOnScreen(event.pageX, event.pageY)
+            );
+            this._zoomEnd.copy(this._zoomStart);
+          }
+          break;
+
+        case this._mouseButtons.RIGHT:
+          this._state = AlTrackballState.PAN;
+          if (!this._noPan) {
+            this._panStart.copy(
+              this._getMouseOnScreen(event.pageX, event.pageY)
+            );
+            this._panEnd.copy(this._panStart);
+          }
+          break;
+
+        default:
+          this._state = AlTrackballState.NONE;
+      }
+    }
+
+    // this.emit(this._startEvent);
+  }
+
+  private _mouseMove(event: MouseEvent) {
+    if (this._enabled === false) {
+      return;
+    }
+
+    if (this._state === AlTrackballState.ROTATE && !this._noRotate) {
+      this._movePrev.copy(this._moveCurr);
+      this._moveCurr.copy(this._getMouseOnCircle(event.pageX, event.pageY));
+    } else if (this._state === AlTrackballState.ZOOM && !this._noZoom) {
+      this._zoomEnd.copy(this._getMouseOnScreen(event.pageX, event.pageY));
+    } else if (this._state === AlTrackballState.PAN && !this._noPan) {
+      this._panEnd.copy(this._getMouseOnScreen(event.pageX, event.pageY));
+    }
+  }
+
+  private _mouseUp(_event: MouseEvent) {
+    if (this._enabled === false) {
+      return;
+    }
+
+    this._state = AlTrackballState.NONE;
+
+    // this.dispatchEvent(this._endEvent);
+  }
+
+  private _mouseWheel(event: WheelEvent) {
+    if (this._enabled === false || this._noZoom === true) {
+      return;
+    }
+    switch (event.deltaMode) {
+      case 2:
+        // Zoom in pages
+        this._zoomStart.y -= event.deltaY * 0.025;
+        break;
+
+      case 1:
+        // Zoom in lines
+        this._zoomStart.y -= event.deltaY * 0.01;
+        break;
+
+      default:
+        // undefined, 0, assume pixels
+        this._zoomStart.y -= event.deltaY * 0.00025;
+        break;
+    }
+
+    // this.dispatchEvent(this._startEvent);
+    // this.dispatchEvent(this._endEvent);
+  }
+
+  private _touchStart(event: TouchEvent) {
+    if (this._enabled === false) {
+      return;
+    }
+    switch (event.touches.length) {
+      case 1:
+        this._state = AlTrackballState.TOUCH_ROTATE;
+        this._moveCurr.copy(
+          this._getMouseOnCircle(event.touches[0].pageX, event.touches[0].pageY)
+        );
+        this._movePrev.copy(this._moveCurr);
+        break;
+
+      default:
+        // 2 or more
+        this._state = AlTrackballState.TOUCH_ZOOM_PAN;
+        const dx = event.touches[0].pageX - event.touches[1].pageX;
+        const dy = event.touches[0].pageY - event.touches[1].pageY;
+        this._touchZoomDistanceEnd = this._touchZoomDistanceStart = Math.sqrt(
+          dx * dx + dy * dy
+        );
+
+        const x = (event.touches[0].pageX + event.touches[1].pageX) / 2;
+        const y = (event.touches[0].pageY + event.touches[1].pageY) / 2;
+        this._panStart.copy(this._getMouseOnScreen(x, y));
+        this._panEnd.copy(this._panStart);
+        break;
+    }
+
+    // this.dispatchEvent(this._startEvent);
+  }
+
+  private _touchEnd(event: TouchEvent) {
+    if (this._enabled === false) {
+      return;
+    }
+
+    switch (event.touches.length) {
+      case 0:
+        this._state = AlTrackballState.NONE;
+        break;
+
+      case 1:
+        this._state = AlTrackballState.TOUCH_ROTATE;
+        this._moveCurr.copy(
+          this._getMouseOnCircle(event.touches[0].pageX, event.touches[0].pageY)
+        );
+        this._movePrev.copy(this._moveCurr);
+        break;
+      default: {
+        break;
+      }
+    }
+
+    // this.dispatchEvent(this._endEvent);
+  }
+
+  private _touchMove(event: TouchEvent) {
+    if (this._enabled === false) {
+      return;
+    }
+
+    switch (event.touches.length) {
+      case 1:
+        this._movePrev.copy(this._moveCurr);
+        this._moveCurr.copy(
+          this._getMouseOnCircle(event.touches[0].pageX, event.touches[0].pageY)
+        );
+        break;
+
+      default:
+        // 2 or more
+        const dx = event.touches[0].pageX - event.touches[1].pageX;
+        const dy = event.touches[0].pageY - event.touches[1].pageY;
+        this._touchZoomDistanceEnd = Math.sqrt(dx * dx + dy * dy);
+
+        const x = (event.touches[0].pageX + event.touches[1].pageX) / 2;
+        const y = (event.touches[0].pageY + event.touches[1].pageY) / 2;
+        this._panEnd.copy(this._getMouseOnScreen(x, y));
+        break;
+    }
+  }
+  public _resize() {
+    if (this._domElement === document) {
+      this._screen.left = 0;
+      this._screen.top = 0;
+      this._screen.width = window.innerWidth;
+      this._screen.height = window.innerHeight;
+    } else {
+      const box = this._domElement.getBoundingClientRect();
+      // adjustments come from similar code in the jquery offset() function
+      const d = this._domElement.ownerDocument.documentElement;
+      this._screen.left = box.left + window.pageXOffset - d.clientLeft;
+      this._screen.top = box.top + window.pageYOffset - d.clientTop;
+      this._screen.width = box.width;
+      this._screen.height = box.height;
+    }
+  }
+
+  private _getMouseOnScreen(pageX: number, pageY: number): THREE.Vector2 {
+    const vector = new THREE.Vector2();
+    vector.set(
+      (pageX - this._screen.left) / this._screen.width,
+      (pageY - this._screen.top) / this._screen.height
+    );
+
+    return vector;
+  }
+
+  private _getMouseOnCircle(pageX: number, pageY: number): THREE.Vector2 {
+    const vector = new THREE.Vector2();
+    vector.set(
+      (pageX - this._screen.width * 0.5 - this._screen.left) /
+        (this._screen.width * 0.5),
+      (this._screen.height + 2 * (this._screen.top - pageY)) /
+        this._screen.width // screen.width intentional
+    );
+    return vector;
+  }
+
+  private _rotateCamera() {
+    const axis = new THREE.Vector3();
+    const quaternion = new THREE.Quaternion();
+    const eyeDirection = new THREE.Vector3();
+    const objectUpDirection = new THREE.Vector3();
+    const objectSidewaysDirection = new THREE.Vector3();
+    const moveDirection = new THREE.Vector3();
+    let angle;
+
+    moveDirection.set(
+      this._moveCurr.x - this._movePrev.x,
+      this._moveCurr.y - this._movePrev.y,
+      0
+    );
+    angle = moveDirection.length();
+
+    if (angle) {
+      this._eye.copy(this.object.position).sub(this.target);
+
+      eyeDirection.copy(this._eye).normalize();
+      objectUpDirection.copy(this.object.up).normalize();
+      objectSidewaysDirection
+        .crossVectors(objectUpDirection, eyeDirection)
+        .normalize();
+
+      objectUpDirection.setLength(this._moveCurr.y - this._movePrev.y);
+      objectSidewaysDirection.setLength(this._moveCurr.x - this._movePrev.x);
+
+      moveDirection.copy(objectUpDirection.add(objectSidewaysDirection));
+
+      axis.crossVectors(moveDirection, this._eye).normalize();
+
+      angle *= this._rotateSpeed;
+      quaternion.setFromAxisAngle(axis, angle);
+
+      this._eye.applyQuaternion(quaternion);
+      this.object.up.applyQuaternion(quaternion);
+
+      this._lastAxis.copy(axis);
+      this._lastAngle = angle;
+    } else if (!this._staticMoving && this._lastAngle) {
+      this._lastAngle *= Math.sqrt(1.0 - this._dynamicDampingFactor);
+      this._eye.copy(this.object.position).sub(this.target);
+      quaternion.setFromAxisAngle(this._lastAxis, this._lastAngle);
+      this._eye.applyQuaternion(quaternion);
+      this.object.up.applyQuaternion(quaternion);
+    }
+
+    this._movePrev.copy(this._moveCurr);
+  }
+
+  private _zoomCamera() {
+    let factor;
+
+    if (this._state === AlTrackballState.TOUCH_ZOOM_PAN) {
+      factor = this._touchZoomDistanceStart / this._touchZoomDistanceEnd;
+      this._touchZoomDistanceStart = this._touchZoomDistanceEnd;
+      this._eye.multiplyScalar(factor);
+    } else {
+      factor = 1.0 + (this._zoomEnd.y - this._zoomStart.y) * this._zoomSpeed;
+
+      if (factor !== 1.0 && factor > 0.0) {
+        this._eye.multiplyScalar(factor);
+      }
+
+      if (this._staticMoving) {
+        this._zoomStart.copy(this._zoomEnd);
+      } else {
+        this._zoomStart.y +=
+          (this._zoomEnd.y - this._zoomStart.y) * this._dynamicDampingFactor;
+      }
+    }
+  }
+
+  private _panCamera() {
+    const mouseChange = new THREE.Vector2();
+    const objectUp = new THREE.Vector3();
+    const pan = new THREE.Vector3();
+    mouseChange.copy(this._panEnd).sub(this._panStart);
+
+    if (mouseChange.lengthSq()) {
+      mouseChange.multiplyScalar(this._eye.length() * this._panSpeed);
+
+      pan
+        .copy(this._eye)
+        .cross(this.object.up)
+        .setLength(mouseChange.x);
+      pan.add(objectUp.copy(this.object.up).setLength(mouseChange.y));
+
+      this.object.position.add(pan);
+      this.target.add(pan);
+
+      if (this._staticMoving) {
+        this._panStart.copy(this._panEnd);
+      } else {
+        this._panStart.add(
+          mouseChange
+            .subVectors(this._panEnd, this._panStart)
+            .multiplyScalar(this._dynamicDampingFactor)
+        );
+      }
+    }
+  }
+
+  private _checkDistances() {
+    if (!this._noZoom || !this._noPan) {
+      if (this._eye.lengthSq() > this._maxDistance * this._maxDistance) {
+        this.object.position.addVectors(
+          this.target,
+          this._eye.setLength(this._maxDistance)
+        );
+        this._zoomStart.copy(this._zoomEnd);
+      }
+
+      if (this._eye.lengthSq() < this._minDistance * this._minDistance) {
+        this.object.position.addVectors(
+          this.target,
+          this._eye.setLength(this._minDistance)
+        );
+        this._zoomStart.copy(this._zoomEnd);
+      }
+    }
+  }
+
+  public update() {
+    this._eye.subVectors(this.object.position, this.target);
+
+    if (!this._noRotate) {
+      this._rotateCamera();
+    }
+    if (!this._noZoom) {
+      this._zoomCamera();
+    }
+    if (!this._noPan) {
+      this._panCamera();
+    }
+
+    this.object.position.addVectors(this.target, this._eye);
+    this._checkDistances();
+    this.object.lookAt(this.target);
+
+    if (this._lastPosition.distanceToSquared(this.object.position) > this.EPS) {
+      //   this.dispatchEvent(this._changeEvent);
+      this._lastPosition.copy(this.object.position);
+    }
+  }
+
+  public reset() {
+    this._state = AlTrackballState.NONE;
+
+    this.target.copy(this._target0);
+    this.object.position.copy(this._position0);
+    this.object.up.copy(this._up0);
+    this._eye.subVectors(this.object.position, this.target);
+    this.object.lookAt(this.target);
+
+    // this.dispatchEvent(this._changeEvent);
+
+    this._lastPosition.copy(this.object.position);
+  }
+
+  public dispose() {
+    this._removeListeners();
+  }
+
+  private _removeListeners() {
+    this._domElement.removeEventListener('mousedown', this._mouseDown, false);
+    this._domElement.removeEventListener('mousemove', this._mouseMove, false);
+    this._domElement.removeEventListener('mouseup', this._mouseUp, false);
+    this._domElement.removeEventListener('wheel', this._mouseWheel, false);
+
+    this._domElement.removeEventListener('touchstart', this._touchStart, false);
+    this._domElement.removeEventListener('touchend', this._touchEnd, false);
+    this._domElement.removeEventListener('touchmove', this._touchMove, false);
+  }
+}
+
+this.handleResize();
+
+// force an update at start
+this.update();
